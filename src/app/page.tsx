aaa96d1ccc287e8015a300047f953f4e4b0daa72
Kuba <jakubwidurek@gmail.com>
2025-04-28 20:30:54 +0200

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -479,7 +479,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
@@ -508,7 +508,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
@@ -527,7 +527,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
@@ -548,7 +548,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
@@ -566,7 +566,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
@@ -584,7 +584,7 @@ export default function Home() {
               classNameNotInView="opacity-0"
             >
               <div
-                className="bg-gray-800 rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
+                className="bg-[#181818] rounded-xl shadow-lg p-6 transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl active:-translate-y-2 active:shadow-2xl focus:-translate-y-2 focus:shadow-2xl cursor-pointer"
                 tabIndex={0}
               >
                 <h3 className="font-bold text-lg md:text-xl text-gray-200 mb-2">
