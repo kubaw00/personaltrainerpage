4670510fb46cc14c850b7f70961d8523a82878ee
Kuba <jakubwidurek@gmail.com>
2025-05-04 15:20:28 +0200

    pop

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import HamburgerButton from "./HumburgerButton";
 import { scrollToSection } from "@/utils/scrollToSection";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function Nav() {
   return (
