b25d31d65c03d1507975d8687b246649bd642246
Kuba <jakubwidurek@gmail.com>
2025-05-19 19:50:10 +0200

    change script to GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Work_Sans, Montserrat, Anton } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 import "./globals.css";
 import "@/styles/animations.css";
 import "@/styles/swiper-overrides.css";
@@ -105,13 +106,15 @@ export default function RootLayout({
     >
       <head>
         {/* Google Tag Manager */}
-        <script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-W6C9K4XB');`}
-        </script>
+          })(window,document,'script','dataLayer','GTM-W6C9K4XB');
+        `}
+        </Script>
         {/* End Google Tag Manager */}
 
         {/* Structured data JSON-LD for LocalBusiness */}
